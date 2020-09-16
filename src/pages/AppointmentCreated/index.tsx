d02d617d9e5b207c30d468f8948bae1d4d5905cb
Fabio Bandacheski <fabio.versao.beta@gmail.com>
2020-09-16 08:30:14 -0300

    tela agendamento criado com sucesso!

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,6 +1,38 @@
-import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import React, { useCallback } from 'react';
 import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-const AppointmentCreated: React.FC = () => <View />;
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
+
+const AppointmentCreated: React.FC = () => {
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
+
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>Terça, dia 14 de março de 2020 às 12:00h</Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>OK</OkButtonText>
+      </OkButton>
+    </Container>
+  );
+};
 
 export default AppointmentCreated;
